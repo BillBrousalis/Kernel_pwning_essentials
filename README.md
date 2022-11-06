276f235494977cd5d7381c6fb287532f9faf0832
BillBrousalis <bill.brousalis@gmail.com>
2022-11-06 18:45:55 +0200

    initial

---
+++ README.md
@@ -0,0 +1,3 @@
+# Essential scripts for pwning kernel challenges
+
+Learning kernel
---
+++ str2hex.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+import sys
+
+def howto():
+  print('Provide string to hexify as argument.\n'
+        'ex. ./str2hex flag.txt\n')
+  exit()
+
+def hexify(string):
+  return ''.join([hex(ord(c))[2:] for c in string[::-1]])
+
+if __name__ == '__main__':
+  if len(sys.argv) < 2: howto()
+  string = sys.argv[1]
+  print(f'{string} --> 0x{hexify(string)}')
